ad1822e08ab1e9481173b000d64751b769aff3d2
jf.cloutier@123loadboard.com <jf.cloutier@123loadboard.com>
2020-02-25 11:14:10 -0500

    Output the timestamp as a JSON property

--- src/main/java/org/one23lb/apim/event/extractor/ApimHttpMessageProcessor.java
+++ src/main/java/org/one23lb/apim/event/extractor/ApimHttpMessageProcessor.java
@@ -20,6 +20,7 @@ public class ApimHttpMessageProcessor extends AvroDataProcessor<GenericRecord>
         root.addProperty("flowId", getStringProperty(record, "flowId"));
         root.addProperty("parentId", getStringProperty(record, "parentId"));
         root.addProperty("opId", getStringProperty(record, "opId"));
+        root.addProperty("ts", getStringProperty(record, "ts"));
 
         EventParser.validate(root, "requestMetadata", getStringProperty(record, "req_metadata"));
         EventParser.validate(root, "requestBody", getStringProperty(record, "req_body"));
